4d774161cb26d5c90185b2723ac2593843e2fcf5
JackShenYt <61526569+sysytwl@users.noreply.github.com>
2023-09-08 18:26:36 +0100

    Add files via upload

--- CameraWebServer.ino
+++ CameraWebServer.ino
@@ -1,12 +1,11 @@
 #include "CameraWebServer.h"
 #include "server_index.cpp"
 #include "login_page.cpp"
+#include "app_httpd.cpp"
 
 
 
 WebServer server(80);
-void startCameraServer();
-void setupLedFlash(int pin);
 
 void setup() {
   Serial.begin(115200);
@@ -34,32 +33,16 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.frame_size = FRAMESIZE_96X96; // 96x96 resolution
-  config.pixel_format = PIXFORMAT_RGB888,    // 3BPP/RGB888 image format
-  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  //config.pixel_format = PIXFORMAT_JPEG, //PIXFORMAT_RGB888,    // 3BPP/RGB888 image format
+  config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
   config.fb_count = 1;
-  
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  if(config.pixel_format == PIXFORMAT_JPEG){
-    if(psramFound()){
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  } else {
-    // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
-  }
+  config.jpeg_quality = 10; // ?
+  config.fb_count = 2; // ?
+  config.grab_mode = CAMERA_GRAB_LATEST; // ?
+
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
@@ -75,19 +58,6 @@ void setup() {
     s->set_brightness(s, 1); // up the brightness just a bit
     s->set_saturation(s, -2); // lower the saturation
   }
-  // drop down frame size for higher initial frame rate
-  if(config.pixel_format == PIXFORMAT_JPEG){
-    s->set_framesize(s, FRAMESIZE_QVGA);
-  }
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-
-#if defined(CAMERA_MODEL_ESP32S3_EYE)
-  s->set_vflip(s, 1);
-#endif
 
 // Setup LED FLash if LED pin is defined in camera_pins.h
 #if defined(LED_GPIO_NUM)
@@ -95,7 +65,6 @@ void setup() {
 #endif
 
 // wifi
-  // Connect to WiFi network
   // AP name,passwd
   WiFi.softAP(host, password);
   
@@ -127,7 +96,7 @@ void setup() {
   }
   Serial.println("mDNS responder started");
   /*return index page which is stored in serverIndex */
-  server.on("/", HTTP_GET, []() {
+  server.on("/login", HTTP_GET, []() {
     server.sendHeader("Connection", "close");
     server.send(200, "text/html", loginIndex);
   });
